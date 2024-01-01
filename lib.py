7034fd96a75c1feec3dfcd27ec6f4f565dd8662e
Aytan-sudo <cantais.aymeric@gmail.com>
2024-01-01 21:54:23 +0100

    Initialise the creation

---
+++ lib.py
@@ -0,0 +1,16 @@
+# File for functions
+
+from typing import List
+
+def sqi(list: List[int]) -> int:
+    """
+    Calculate the Shuffle Quality Index (SQI)
+    This index defines the quality of the blend.
+    "1" is the worst quality (sequential digits)
+    The higher the SQI, the better.
+    """
+
+    for i in list:
+        pass
+
+    return 1
