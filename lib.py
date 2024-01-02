81ec50643ae9b688264672319c0d16ed1b69e640
Aytan-sudo <cantais.aymeric@gmail.com>
2024-01-02 11:08:36 +0100

    first algo

--- lib.py
+++ lib.py
@@ -1,16 +1,26 @@
 # File for functions
 
 from typing import List
+import random
 
-def sqi(list: List[int]) -> int:
+def sqi(deck: List[int]) -> int:
     """
     Calculate the Shuffle Quality Index (SQI)
     This index defines the quality of the blend.
     "1" is the worst quality (sequential digits)
     The higher the SQI, the better.
+    This function is for testing sequential integers from 1 to n only.
     """
 
-    for i in list:
-        pass
+    sqi = 0
+    for i in range(len(deck)-1):
+        sqi += abs(deck[i+1]-deck[i])
+    sqi = sqi / (len(deck)-1)
+    return sqi
 
-    return 1
+
+def fisher_yates(deck: list) -> list:
+    for i in range(len(deck)-1):
+        j = random.randint(0,i)
+        deck[i], deck[j] = deck[j], deck[i]
+    return deck
--- shuffle.py
+++ shuffle.py
@@ -1,2 +1,14 @@
-import lib.py
+import lib
+import timeit
 
+n=10
+deck = [i for i in range(1, n+1)]
+print(deck)
+print(lib.sqi(deck))
+
+deck2 = lib.fisher_yates(deck)
+print(deck2)
+print(lib.sqi(deck2))
+
+temps = timeit.timeit("lib.fisher_yates(deck)", setup="from __main__ import ma_fonction", number=1000)
+print(f"Temps d'exécution : {temps} secondes")
